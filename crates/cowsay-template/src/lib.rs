6d2c43912475d0b9ca83810cab9b1b68ce8616d7
Franco Grobler <franco@grobler.fyi>
2025-11-03 23:15:42 +0200

    feat(template): basic template parser complete

--- crates/cowsay-template/src/lib.rs
+++ crates/cowsay-template/src/lib.rs
@@ -1,9 +1,17 @@
+use std::collections::HashMap;
 use std::path::Path;
 
+use crate::errors::ParseError;
+use crate::loader::{load_cow, load_template};
+
+mod errors;
+mod loader;
+
 #[derive(Debug)]
-pub struct CowTemplate<'a> {
-    path: &'a Path,
-    variables: Vec<String>,
+pub struct CowTemplate {
+    raw_content: String,
+    cow: String,
+    variables: HashMap<String, String>,
 }
 
 #[derive(Debug)]
@@ -12,23 +20,53 @@ pub struct CowTemplateResult {
     pub description: String,
 }
 
-impl<'a> CowTemplate<'a> {
-    pub fn new(path: &'a Path, variables: Vec<String>) -> Self {
-        CowTemplate { path, variables }
+impl CowTemplate {
+    pub fn new(
+        path: &Path,
+        variables: HashMap<String, String>,
+    ) -> Result<Self, ParseError> {
+        let raw_content = load_template(path)?;
+        let cow = load_cow(raw_content.as_str())?;
+        Ok(CowTemplate {
+            variables,
+            raw_content,
+            cow,
+        })
     }
 
-    pub fn render(&self) -> String {
-        // Placeholder implementation
-        format!(
-            "Rendering template from {:?} with variables: {:?}",
-            self.path, self.variables
-        )
+    pub fn render(self) -> String {
+        let variable_regex =
+            regex::Regex::new(r"\$\{?(\w+)}?").expect("Regex is fucked");
+        let mut rendered_content = self.cow.clone();
+
+        variable_regex
+            .captures_iter(rendered_content.clone().as_str())
+            .for_each(|cap| {
+                let instance_name = cap.get(0);
+                if let Some(var_name) = cap.get(1) {
+                    let var_name_str = var_name.as_str();
+                    if let Some(value) = self.variables.get(var_name_str) {
+                        rendered_content = rendered_content
+                            .replace(instance_name.unwrap().as_str(), value);
+                    }
+                }
+            });
+        rendered_content
     }
 
-    pub fn render_with_description(&self) -> CowTemplateResult {
+    pub fn render_with_description(self) -> CowTemplateResult {
+        let description_regex =
+            regex::Regex::new(r"^##(.*)").expect("Regex is fucked");
+        let description = if let Some(caps) =
+            description_regex.captures(self.raw_content.as_str())
+        {
+            caps.get(1).map_or("", |m| m.as_str()).trim().to_string()
+        } else {
+            String::from("No description available")
+        };
         CowTemplateResult {
             rendered: self.render(),
-            description: String::from("Rendered cow template"),
+            description,
         }
     }
 }
@@ -46,7 +84,7 @@ mod tests {
         let test_cow = temp_dir.path().join("test.cow");
         let cow_file = File::create(&test_cow).unwrap();
         let lines = [
-            "$the_cow = <<\"EOC\";",
+            r#"$the_cow = <<"EOC";"#,
             "        $thoughts   ^__^",
             r"         $thoughts  ($eyes)\_______",
             r"            (__)\       )\/\",
@@ -58,10 +96,14 @@ mod tests {
             writeln!(&cow_file, "{}", line).unwrap();
         }
 
-        let cow = CowTemplate::new(
-            test_cow.as_path(),
-            vec![String::from(r"\"), String::from("oo"), String::from("")],
-        );
+        let variables = HashMap::from([
+            ("thoughts".to_string(), r"\".to_string()),
+            ("eyes".to_string(), "oo".to_string()),
+            ("tongue".to_string(), "  ".to_string()),
+        ]);
+        let cow = CowTemplate::new(test_cow.as_path(), variables)
+            .expect("Creating CowTemplate failed");
+
         let output = cow.render();
         let expected_output = [
             r"        \   ^__^",
@@ -72,8 +114,8 @@ mod tests {
         ]
         .join("\n");
 
-        println!("Output:\n{}", output);
         assert_eq!(expected_output, output);
+
         drop(cow_file);
         temp_dir.close().unwrap();
     }
