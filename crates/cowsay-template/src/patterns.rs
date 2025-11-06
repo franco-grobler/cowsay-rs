1c17084f2f2ac4820684348a235e7d103a48e4a1
Franco Grobler <franco@grobler.fyi>
2025-11-06 22:41:22 +0200

    refactor(template): use once lock for compiling loader regex

--- crates/cowsay-template/src/patterns.rs
+++ crates/cowsay-template/src/patterns.rs
@@ -2,10 +2,26 @@ use std::sync::OnceLock;
 
 use regex::Regex;
 
+pub fn get_cow_regex() -> &'static Regex {
+    static RE: OnceLock<Regex> = OnceLock::new();
+
+    RE.get_or_init(|| {
+        Regex::new(r#"\$the_cow\s*=\s*<<"*EOC"*;*\n([\s\S]*)\nEOC"#).unwrap()
+    })
+}
+
 pub fn get_description_regex() -> &'static Regex {
     static RE: OnceLock<Regex> = OnceLock::new();
 
     RE.get_or_init(|| Regex::new(r"^##(.*)").unwrap())
+}
+
+pub fn get_substitution_regex() -> &'static Regex {
+    static RE: OnceLock<Regex> = OnceLock::new();
+
+    RE.get_or_init(|| {
+        Regex::new(r#"\\([\\\.\+\*\?\(\)\|\[\]\{\}\^\$\#&\-~@])"#).unwrap()
+    })
 }
 
 pub fn get_variable_regex() -> &'static Regex {
