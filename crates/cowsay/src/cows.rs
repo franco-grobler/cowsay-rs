11e3d05232b63b26c230a42d0b51936bdff34646
Franco Grobler <franco@grobler.fyi>
2025-11-13 20:42:15 +0200

    docs(cowsay): add cows docs

--- crates/cowsay/src/cows.rs
+++ crates/cowsay/src/cows.rs
@@ -2,13 +2,41 @@ use crate::errors::CowsayError;
 use cowsay_template::errors::ParseError;
 use include_directory::{Dir, include_directory};
 
+use rand::rng;
 use rand::seq::IteratorRandom;
 
 static COWS: Dir<'_> = include_directory!("$CARGO_MANIFEST_DIR/../../cows");
 
-/// Choose a random cowfile from the built-in cows.
+/// Selects a random cowfile from the embedded cowfiles.
+///
+/// This function reads the list of available cowfiles and randomly selects one.
+/// The content of the selected cowfile is then returned as a `String`.
+///
+/// # Returns
+///
+/// A `Result` which is:
+/// - `Ok(String)` containing the content of a randomly selected cowfile.
+/// - `Err(CowsayError::CowfileNotFound)` if no cowfiles are found.
+/// - `Err(CowsayError::CowfileParseError)` if the selected cowfile cannot be
+///   read or is not valid UTF-8.
+///
+/// # Panics
+///
+/// This function panics if `file_name().unwrap()` is called on a `None` value,
+/// which can happen if a directory entry does not have a valid file name.
+/// This is highly unlikely with the embedded cowfiles, but theoretically possible
+/// if the `include_directory!` macro includes malformed entries.
+///
+/// # Examples
+///
+/// ```
+/// use cowsay::cows;
+///
+/// let random_cow_template = cows::get_random_cow().unwrap();
+/// println!("{}", random_cow_template);
+/// ```
 pub fn get_random_cow() -> Result<String, CowsayError> {
-    let mut rng = rand::rng();
+    let mut rng = rng();
     let file_name = COWS
         .files()
         .map(|f| f.path().file_name().unwrap().to_string_lossy().to_string())
@@ -19,19 +47,46 @@ pub fn get_random_cow() -> Result<String, CowsayError> {
     get_cow_from_file(&file_name)
 }
 
-/// Read a cowfile from the built-in cows.
+/// Reads the content of a specified cowfile from the embedded cowfiles.
+///
+/// This function attempts to find and read the content of a cowfile
+/// identified by `file_name`. The content is returned as a `String`.
+///
+/// # Arguments
+///
+/// * `file_name` - The name of the cowfile to retrieve (e.g., "default.cow").
+///
+/// # Returns
+///
+/// A `Result` which is:
+/// - `Ok(String)` containing the content of the specified cowfile.
+/// - `Err(CowsayError::CowfileNotFound)` if the specified cowfile does not exist.
+/// - `Err(CowsayError::CowfileParseError)` if the cowfile cannot be read
+///   or is not valid UTF-8.
+///
+/// # Examples
+///
+/// ```
+/// use cowsay::cows;
+///
+/// let default_cow_template = cows::get_cow_from_file("default.cow").unwrap();
+/// println!("{}", default_cow_template);
+/// ```
 pub fn get_cow_from_file(file_name: &str) -> Result<String, CowsayError> {
     let file = COWS.get_file(file_name);
-    match file {
-        Some(f) => match f.contents_utf8() {
-            Some(contents) => Ok(contents.to_string()),
-            None => Err(CowsayError::CowfileParseError(
-                ParseError::InvalidTemplateFormat(format!(
-                    "cowfile '{}' is not valid UTF-8",
-                    file_name
-                )),
-            )),
+    file.map_or_else(
+        || Err(CowsayError::CowfileNotFound(file_name.to_string())),
+        |f| {
+            f.contents_utf8().map_or_else(
+                || {
+                    Err(CowsayError::CowfileParseError(
+                        ParseError::InvalidTemplateFormat(format!(
+                            "cowfile '{file_name}' is not valid UTF-8"
+                        )),
+                    ))
+                },
+                |contents| Ok(contents.to_string()),
+            )
         },
-        None => Err(CowsayError::CowfileNotFound(file_name.to_string())),
-    }
+    )
 }
