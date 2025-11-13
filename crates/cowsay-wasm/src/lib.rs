d1846d8adcf673359abd5d789a2e7d5f7f7b75e4
Franco Grobler <franco@grobler.fyi>
2025-11-13 21:29:44 +0200

    fix(wasm): create a option constructor struct for options constructor

--- crates/cowsay-wasm/src/lib.rs
+++ crates/cowsay-wasm/src/lib.rs
@@ -1,5 +1,3 @@
-//! WebAssembly version of cowsay.
-
 use cowsay::CowsayOption;
 use js_sys::Error;
 use wasm_bindgen::{JsValue, prelude::wasm_bindgen};
@@ -42,32 +40,67 @@ pub struct Options {
     wrap_column: Option<usize>,
 }
 
+#[derive(Debug, serde::Deserialize)]
+#[allow(clippy::struct_excessive_bools)]
+#[wasm_bindgen]
+/// Options cowsay accepts.
+///
+/// * `borg`: Borg appearance.
+/// * `dead`: Dead appearance.
+/// * `greedy`: Greedy appearance.
+/// * `sleepy`: Sleepy appearance.
+/// * `tired`: Tired appearance.
+/// * `wired`: Wired appearance.
+/// * `young`: Young appearance.
+/// * `file`: Template file name, or template string.
+/// * `random`: Use a random template.
+/// * `eyes`: Characters to use for the eyes.
+/// * `tongue`: Characters to use for the tongue.
+/// * `wrap`: Should the text be wrapped?
+/// * `wrap_column`: Column width to wrap text at.
+pub struct OptionsConstructor {
+    borg: Option<bool>,
+    dead: Option<bool>,
+    greedy: Option<bool>,
+    sleepy: Option<bool>,
+    tired: Option<bool>,
+    wired: Option<bool>,
+    young: Option<bool>,
+    file: Option<String>,
+    random: Option<bool>,
+    eyes: Option<String>,
+    tongue: Option<String>,
+    wrap: Option<bool>,
+    wrap_column: Option<usize>,
+}
+
 #[wasm_bindgen]
 impl Options {
     #[wasm_bindgen(constructor)]
     pub fn new(options: JsValue) -> Result<Self, Error> {
-        let options: Options =
+        let options: OptionsConstructor =
             serde_wasm_bindgen::from_value(options).map_err(into_error)?;
 
         Ok(Self {
-            borg: options.borg,
-            dead: options.dead,
-            greedy: options.greedy,
-            sleepy: options.sleepy,
-            tired: options.tired,
-            wired: options.wired,
-            young: options.young,
+            borg: options.borg.unwrap_or(false),
+            dead: options.dead.unwrap_or(false),
+            greedy: options.greedy.unwrap_or(false),
+            sleepy: options.sleepy.unwrap_or(false),
+            tired: options.tired.unwrap_or(false),
+            wired: options.wired.unwrap_or(false),
+            young: options.young.unwrap_or(false),
             file: options.file,
-            random: options.random,
+            random: options.random.unwrap_or(false),
             eyes: options.eyes,
             tongue: options.tongue,
-            wrap: options.wrap,
+            wrap: options.wrap.unwrap_or(false),
             wrap_column: options.wrap_column,
         })
     }
 
     #[wasm_bindgen(constructor)]
-    pub fn default() -> Self {
+    #[allow(clippy::missing_const_for_fn)]
+    pub fn default_options() -> Self {
         Self {
             borg: false,
             dead: false,
@@ -85,8 +118,8 @@ impl Options {
         }
     }
 
-    fn to_cowsay_option(&self) -> CowsayOption {
-        CowsayOption::builder()
+    fn to_cowsay_option(&self) -> CowsayOption<'_> {
+        let mut builder = CowsayOption::builder()
             .with_borg(self.borg)
             .with_dead(self.dead)
             .with_greedy(self.greedy)
@@ -94,27 +127,41 @@ impl Options {
             .with_tired(self.tired)
             .with_wired(self.wired)
             .with_young(self.young)
-            .with_file(self.file.clone())
             .with_random(self.random)
-            .with_eyes(self.eyes.clone())
-            .with_tongue(self.tongue.clone())
-            .with_wrap(self.wrap)
-            .with_wrap_column(self.wrap_column)
-            .build()
+            .with_wrap(self.wrap);
+
+        if self.file.is_some() {
+            let file = self.file.as_ref().unwrap();
+            builder = builder.with_cowfile(file.as_str());
+        }
+
+        if self.eyes.is_some() {
+            let eyes = self.eyes.as_ref().unwrap();
+            builder = builder.with_eyes(eyes);
+        }
+        if self.tongue.is_some() {
+            let tongue = self.tongue.as_ref().unwrap();
+            builder = builder.with_tongue(tongue);
+        }
+
+        if self.wrap_column.is_some() {
+            builder = builder
+                .with_wrap(true)
+                .with_wrap_column(self.wrap_column.unwrap());
+        }
+
+        builder.build()
     }
 
     pub fn say(&self, message: &str) -> Result<String, Error> {
         let cowsay_option = self.to_cowsay_option();
-        let parser = match cowsay_option.parser() {
-            Ok(p) => p,
-            Err(e) => into_error(e),
-        };
+        let parser = cowsay_option.parser().map_err(into_error)?;
 
         if message.is_empty() {
-            Err(into_error("Message cannot be empty"))
+            Error::new("Message cannot be empty");
         }
 
-        let cow = parser.say(message);
+        let cow = parser.say(Some(message));
         Ok(cow)
     }
 }
