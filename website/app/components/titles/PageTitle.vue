450bcd8e98823308d958f7701a8379a40ff863da
Franco Grobler <franco@grobler.fyi>
2025-11-21 10:23:51 +0200

    feat(website): switch to bun-nuxt
    Signed-off-by: Franco Grobler <franco@grobler.fyi>

---
+++ website/app/components/titles/PageTitle.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1
+    class="text-text-primary text-4xl font-extrabold leading-tight tracking-[-0.033em] md:text-6xl"
+  >
+    <slot />
+  </h1>
+</template>
---
+++ website/app/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="text-center">
+    <IntroductionView />
+    <PlaygroundView />
+  </div>
+</template>
+
+<script setup lang="ts">
+import IntroductionView from "@/components/views/IntroductionView.vue";
+import PlaygroundView from "@/components/views/PlaygroundView.vue";
+</script>
