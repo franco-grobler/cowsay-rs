220ae898133d4c3293356812f80c241006bff16b
Franco Grobler <franco@grobler.fyi>
2025-11-23 20:52:28 +0200

    feat(website): add placeholder footer
    Signed-off-by: Franco Grobler <franco@grobler.fyi>

---
+++ website/app/components/layout/AppFooter.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer
+    class="flex flex-row items-center justify-between whitespace-nowrap mt-12 py-4"
+  >
+    <div>This is an unlicensed project.</div>
+    <div class="flex flex-row justify-end-safe w-full h-full">
+      <CurrencyEuroIcon />
+    </div>
+  </footer>
+</template>
+
+<script setup lang="ts">
+import { CurrencyEuroIcon } from "@heroicons/vue/16/solid";
+</script>
