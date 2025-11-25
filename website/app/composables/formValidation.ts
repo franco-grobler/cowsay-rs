d6603445875778f48d5365ffe9d126062012dac4
Franco Grobler <franco@grobler.fyi>
2025-11-25 22:02:30 +0200

    chore(website): linting fixes
    Signed-off-by: Franco Grobler <franco@grobler.fyi>

--- website/app/composables/formValidation.ts
+++ website/app/composables/formValidation.ts
@@ -1,3 +1,3 @@
 export type ValidationRule<T> = (value: T) => boolean | string;
 
-export default function useFormValidation(form: HTMLFormElement) {}
+export default function useFormValidation(_form: HTMLFormElement) {}
