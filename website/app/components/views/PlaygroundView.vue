51493c2ead81479465206c0349e0b0ee6c6eb81f
Franco Grobler <franco@grobler.fyi>
2025-12-01 20:43:54 +0200

    feat(website): update playground view
    Signed-off-by: Franco Grobler <franco@grobler.fyi>

--- website/app/components/views/PlaygroundView.vue
+++ website/app/components/views/PlaygroundView.vue
@@ -5,27 +5,36 @@
       WebAssembly allows for JavaScript bindings to the Rust application!
     </p>
     <div class="flex flex-row w-full justify-between gap-5">
-      <form class="flex flex-col gap-3">
+      <form class="flex flex-col gap-3 max-w-[200px]">
         <TextField
-          v-model="cowData.eyes"
+          v-model="eyes"
           label="Eyes"
           placeholder="Eye characters"
+          :validation-rules="[twoLetterRule]"
         />
         <TextField
-          v-model="cowData.tongue"
+          v-model="tongue"
           label="Tongue"
           placeholder="Tongue characters"
+          :validation-rules="[twoLetterRule]"
+        />
+        <TextField v-model="text" label="Text" placeholder="Hello, World!" />
+        <SelectField v-model="file" label="Cowfile" :items="availableCows" />
+        <SwitchField v-model="wrap" label="Wrap" />
+        <NumericField
+          v-model="wrapColumn"
+          label="Wrap width"
+          placeholder="Wrap column"
         />
       </form>
-      <div class="flex flex-col gap-3 min-w-[300px]">
-        <TextField v-model="text" label="Text" placeholder="Hello, World!" />
-        <TextArea v-model="template" label="Cowfile" rows="6" />
-      </div>
-      <div>
+      <div class="flex flex-col grow gap-3 min-w-[300px]">
         <TextArea
           v-model="cow"
           label="What the cow has to say:"
-          class="h-full"
+          class="h-full w-full"
+          readonly
+          :rows="textRows"
+          style="font-family: monospace; field-sizing: content"
         />
       </div>
     </div>
@@ -35,17 +44,41 @@
 <script setup lang="ts">
 import TextField from "@/components/fields/TextField.vue";
 import TextArea from "@/components/fields/TextArea.vue";
+import SelectField from "@/components/fields/SelectField.vue";
+import SwitchField from "@/components/fields/SwitchField.vue";
+import NumericField from "@/components/fields/NumericField.vue";
 import SectionTitle from "@/components/titles/SectionTitle.vue";
-import { ref } from "vue";
+import { computed, ref } from "vue";
+import useCowsay, { type CowsayOptions } from "@/composables/cowsay";
 
-const cowData = ref<{ eyes?: string; tongue?: string }>({});
-const template = ref<string>(`$the_cow = <<"EOC";
-        $thoughts   ^__^
-         $thoughts  ($eyes)\\_______
-            (__)\\       )\\/\\
-             $tongue ||----w |
-                ||     ||
-EOC`);
-const cow = ref<string>("");
-const text = ref<string>();
+const eyes = ref<string>("oo");
+const tongue = ref<string>("U ");
+const text = ref<string>("Hello, World!");
+const file = ref<string>("default");
+const wrap = ref<boolean>(true);
+const wrapColumn = ref<number>(40);
+
+const twoLetterRule = (value: string) =>
+  value.length > 2 ? "Cannot be more than two letters." : false;
+
+const cowData = computed<CowsayOptions>(() => {
+  return {
+    eyes: eyes.value.padEnd(2, " "),
+    tongue: tongue.value.padEnd(2, " "),
+    file: file.value,
+    wrap: wrap.value,
+    wrap_column: wrapColumn.value,
+  };
+});
+const cow = computed<string>(() =>
+  getCowText(text.value.trim(), cowData.value),
+);
+
+const textRows = computed<number>(() =>
+  Math.max(8, cow.value.split("\n").filter((v) => v.trim().length > 0).length),
+);
+
+const availableCows = computed<string[]>(() => getAvailableCows());
+
+const { getAvailableCows, getCowText } = useCowsay();
 </script>
