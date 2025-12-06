581f2dda2e5de6cce08090e39d8190db14f7aacf
Franco Grobler <franco@grobler.fyi>
2025-12-06 07:34:36 +0200

    fix(website): cleanup redundant classes
    Signed-off-by: Franco Grobler <franco@grobler.fyi>

--- website/app/components/titles/SectionTitle.vue
+++ website/app/components/titles/SectionTitle.vue
@@ -1,7 +1,5 @@
 <template>
-  <h2
-    class="text-text-primary text-3xl font-bold leading-tight tracking-tight md:text-4xl"
-  >
+  <h2 class="text-3xl font-bold leading-tight tracking-tight md:text-4xl">
     <slot />
   </h2>
 </template>
