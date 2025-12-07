0adb96948c50fec2a358430b7b3269b4fca73ede
Franco Grobler <franco@grobler.fyi>
2025-12-07 19:14:03 +0200

    fix(website): formatting
    Signed-off-by: Franco Grobler <franco@grobler.fyi>

--- website/app/composables/errors.ts
+++ website/app/composables/errors.ts
@@ -4,36 +4,36 @@ import { computed, type Ref } from "vue";
 const NO_MESSAGE = "__ERROR_STATE__";
 
 export default function useComputedErrors<T>(
-	modelValue: Ref<T>,
-	rules: ValidationRule<T>[],
-	defaultErrors: string[],
+  modelValue: Ref<T>,
+  rules: ValidationRule<T>[],
+  defaultErrors: string[],
 ) {
-	const raw = computed<string[]>(() => {
-		const errors = [] as string[];
-		errors.push(...defaultErrors);
-		const value = modelValue.value;
+  const raw = computed<string[]>(() => {
+    const errors = [] as string[];
+    errors.push(...defaultErrors);
+    const value = modelValue.value;
 
-		for (const rule of rules) {
-			const rsp = rule(value);
-			if (typeof rsp === "boolean" && rsp == true) {
-				errors.push(NO_MESSAGE);
-			} else if (typeof rsp === "string") {
-				errors.push(rsp);
-			}
-		}
+    for (const rule of rules) {
+      const rsp = rule(value);
+      if (typeof rsp === "boolean" && rsp == true) {
+        errors.push(NO_MESSAGE);
+      } else if (typeof rsp === "string") {
+        errors.push(rsp);
+      }
+    }
 
-		return errors;
-	});
+    return errors;
+  });
 
-	const state = computed<boolean>(() => raw.value.length > 0);
+  const state = computed<boolean>(() => raw.value.length > 0);
 
-	const messages = computed<string[]>(() =>
-		raw.value.filter((v) => v !== NO_MESSAGE),
-	);
+  const messages = computed<string[]>(() =>
+    raw.value.filter((v) => v !== NO_MESSAGE),
+  );
 
-	return {
-		raw,
-		state,
-		messages,
-	};
+  return {
+    raw,
+    state,
+    messages,
+  };
 }
