d543ce801f025a66200676ebd407371f73b02c25
Franco Grobler <franco@grobler.fyi>
2025-12-07 19:14:03 +0200

    fix(website): formatting
    Signed-off-by: Franco Grobler <franco@grobler.fyi>

--- website/app/composables/cowsay.ts
+++ website/app/composables/cowsay.ts
@@ -2,50 +2,50 @@ import init, { Options, listCows } from "cowsay-wasm";
 import { ref } from "vue";
 
 export interface CowsayOptions {
-	borg?: boolean;
-	dead?: boolean;
-	greedy?: boolean;
-	sleepy?: boolean;
-	tired?: boolean;
-	wired?: boolean;
-	young?: boolean;
-	file?: string;
-	random?: boolean;
-	eyes?: string;
-	tongue?: string;
-	wrap?: boolean;
-	wrap_column?: string;
+  borg?: boolean;
+  dead?: boolean;
+  greedy?: boolean;
+  sleepy?: boolean;
+  tired?: boolean;
+  wired?: boolean;
+  young?: boolean;
+  file?: string;
+  random?: boolean;
+  eyes?: string;
+  tongue?: string;
+  wrap?: boolean;
+  wrap_column?: string;
 }
 
 export default function useCowsay() {
-	const initialised = ref<boolean>(false);
+  const initialised = ref<boolean>(false);
 
-	async function initCowsay() {
-		if (initialised.value === true) return;
-		await init();
-	}
+  async function initCowsay() {
+    if (initialised.value === true) return;
+    await init();
+  }
 
-	function getCowText(text: string, data: CowsayOptions): string {
-		if (!initialised.value) {
-			return "Not ready...";
-		}
-		const options = new Options({
-			...data,
-			wrap_column: data.wrap ? data.wrap_column : undefined,
-		});
-		return options.say(text);
-	}
+  function getCowText(text: string, data: CowsayOptions): string {
+    if (!initialised.value) {
+      return "Not ready...";
+    }
+    const options = new Options({
+      ...data,
+      wrap_column: data.wrap ? data.wrap_column : undefined,
+    });
+    return options.say(text);
+  }
 
-	function getAvailableCows(): string[] {
-		if (!initialised.value) {
-			return [];
-		}
-		return listCows();
-	}
+  function getAvailableCows(): string[] {
+    if (!initialised.value) {
+      return [];
+    }
+    return listCows();
+  }
 
-	initCowsay().then(() => {
-		initialised.value = true;
-	});
+  initCowsay().then(() => {
+    initialised.value = true;
+  });
 
-	return { initCowsay, initialised, getAvailableCows, getCowText };
+  return { initCowsay, initialised, getAvailableCows, getCowText };
 }
