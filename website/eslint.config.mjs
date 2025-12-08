2032bfaf8ad3a81b3b23ca6f751dc1cc7e54a7e9
Franco Grobler <franco@grobler.fyi>
2025-12-08 20:15:32 +0200

    feat(website): add eslint import sorter
    Signed-off-by: Franco Grobler <franco@grobler.fyi>

--- website/eslint.config.mjs
+++ website/eslint.config.mjs
@@ -1,8 +1,18 @@
 // @ts-check
-import withNuxt from "./.nuxt/eslint.config.mjs";
-
 import eslintConfigPrettier from "eslint-config-prettier";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+
+import withNuxt from "./.nuxt/eslint.config.mjs";
 
 export default withNuxt()
 	.prepend({ ignores: ["vendored/**"] })
+	.append({
+		plugins: {
+			"simple-import-sort": simpleImportSort,
+		},
+		rules: {
+			"simple-import-sort/imports": "error",
+			"simple-import-sort/exports": "error",
+		},
+	})
 	.append(eslintConfigPrettier);
