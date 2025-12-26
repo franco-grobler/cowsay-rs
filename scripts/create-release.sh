3d495c55164ffd8faefac40d510511fbe8ae155e
Franco Grobler <franco@grobler.fyi>
2025-12-26 14:08:25 +0200

    fix: release script
    Signed-off-by: Franco Grobler <franco@grobler.fyi>

--- scripts/create-release.sh
+++ scripts/create-release.sh
@@ -2,7 +2,24 @@
 set -e
 
 root="$(dirname "$0")/.."
+changelog_file='CHANGELOG.md'
 
-git-cliff --bump --config "${root}/cliff.toml"
-git add "${root}/CHANGELOG.md"
-git commit -m "chore(release): add changelog" -m 'prepare for release'
+version=$(git-cliff --bumped-version --config="${root}/cliff.toml")
+commit_msg="chore(release): prepare release version ${version}"
+
+sed -Ei "s/^version = .*$/version = \"${version#v}\"/" "${root}/Cargo.toml"
+
+git-cliff --bump \
+	--config "${root}/cliff.toml" \
+	--with-commit "${commit_msg}" \
+	--output="${changelog_file}"
+
+git add "${root}/${changelog_file}"
+git add "${root}/Cargo.toml"
+
+git commit -m "${commit_msg}"
+git tag -s -a "v$version" -m "Release $version" -m "$(cat "${root}/${changelog_file}")"
+git tag -v "v${version}"
+
+git push
+git push --tags
