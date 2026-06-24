6c7419f9fcd148e8de2e08aafcf7cef47d7deca2
Franco Grobler <franco@grobler.fyi>
2026-06-25 01:42:30 +0200

    feat(interpreter): primary parser implementation
    Signed-off-by: Franco Grobler <franco@grobler.fyi>

--- crates/cowsay-interpreter/src/parser/expressions.rs
+++ crates/cowsay-interpreter/src/parser/expressions.rs
@@ -2,7 +2,7 @@ use super::core::Parser;
 use crate::{
     ast::{
         expressions::{Expression, Literal},
-        token::Type,
+        token::{Token, Type},
     },
     number::Number,
     result::RuntimeError,
@@ -80,24 +80,28 @@ impl Parser<'_> {
         if self.match_token(&[Type::KeywordNil]) {
             return Ok(Expression::Literal(Literal::Nil));
         }
+        if self.match_token(&[Type::Variable]) {
+            let token = self.previous();
+            let name =
+                self.source[token.span.start + 1..token.span.end].to_string();
+            return Ok(Expression::Variable { name, token });
+        }
 
-        if self.match_token(&[Type::LiteralNumber, Type::LiteralString]) {
+        if self.match_token(&[
+            Type::LiteralNumber,
+            Type::LiteralString,
+            Type::LiteralStringInterpolationStart,
+            Type::Redirect,
+        ]) {
             let token = self.previous();
             let raw_text = &self.source[token.span.start..token.span.end];
 
             if token.typ == Type::LiteralNumber {
-                let val: f64 = if let Ok(x) = raw_text.parse() {
-                    x
-                } else {
-                    let err = self.add_error(
-                        format!(
-                            "Could not parse as numeric value: {raw_text}."
-                        ),
-                        token.span,
-                    );
-                    return Err(err);
-                };
-                return Ok(Expression::Literal(Literal::Number(Number(val))));
+                return self.match_number(token, raw_text);
+            } else if token.typ == Type::LiteralStringInterpolationStart {
+                return self.match_interpolated_string(token);
+            } else if token.typ == Type::Redirect {
+                return self.match_heredoc();
             }
 
             return Ok(Expression::Literal(Literal::String(
@@ -119,10 +123,83 @@ impl Parser<'_> {
         let token = self.peek();
         let raw_text = &self.source[token.span.start..token.span.end];
         let err = self.add_error(
-            format!("Syntax Error: Unexpected token {raw_text}"),
+            format!(
+                "Syntax Error: Unexpected token {raw_text} at {} of type {:?}",
+                self.current, token.typ
+            ),
             token.span,
         );
         self.synchronize();
         Err(err)
+    }
+
+    fn match_number(
+        &mut self,
+        token: Token,
+        raw_text: &str,
+    ) -> Result<Expression, RuntimeError> {
+        let val: f64 = if let Ok(x) = raw_text.parse() {
+            x
+        } else {
+            let err = self.add_error(
+                format!("Could not parse as numeric value: {raw_text}."),
+                token.span,
+            );
+            return Err(err);
+        };
+        Ok(Expression::Literal(Literal::Number(Number(val))))
+    }
+
+    fn match_interpolated_string(
+        &mut self,
+        token: Token,
+    ) -> Result<Expression, RuntimeError> {
+        let mut parts: Vec<Expression> = Vec::new();
+        while !self.is_at_end() {
+            let expr = match self.advance().typ {
+                Type::LiteralStringInterpolationEnd => break,
+                _ => self.primary(),
+            };
+            parts.push(expr?);
+        }
+        if self.is_at_end() {
+            let err = self.add_error(
+                "Could not find end of interpolated string.".to_string(),
+                token.span,
+            );
+            return Err(err);
+        }
+        Ok(Expression::InterpolatedString {
+            parts,
+            is_heredoc: false,
+        })
+    }
+
+    fn match_heredoc(&mut self) -> Result<Expression, RuntimeError> {
+        let mut parts: Vec<Expression> = Vec::new();
+        self.consume(
+            Type::Identifier,
+            "Expected identifier after heredoc.".to_string(),
+        )?;
+        self.consume(
+            Type::Semicolon,
+            "Expected semicolon after heredoc identifier.".to_string(),
+        )?;
+        loop {
+            if self.is_at_end() || self.match_token(&[Type::Identifier]) {
+                break;
+            }
+            let expr = self.primary()?;
+            parts.push(expr);
+        }
+        self.consume(
+            Type::RedirectEnd,
+            "Expected semicolon after heredoc identifier.".to_string(),
+        )?;
+
+        Ok(Expression::InterpolatedString {
+            parts,
+            is_heredoc: true,
+        })
     }
 }
